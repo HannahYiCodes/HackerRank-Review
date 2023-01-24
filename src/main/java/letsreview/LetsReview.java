cdff4cb76824bd00120018f53284bdb3d08fdaad
HannahYiCodes <hannahyi.student@careerdevs.com>
2023-01-23 23:17:33 -0500

    Let's Review

---
+++ src/main/java/letsreview/LetsReview.java
@@ -0,0 +1,25 @@
+package letsreview;
+
+import java.io.*;
+import java.util.*;
+
+public class LetsReview {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            for (int j = 0; j < str.length(); j+=2) {
+                System.out.print(str.charAt(j));
+            }
+            System.out.print(" ");
+            for (int j = 1; j < str.length(); j+=2) {
+                System.out.print(str.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
